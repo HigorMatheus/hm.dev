8a528d68c9d90bad427be7769e2af21818a38cc7
Higor Matheus <hmoliveira83@gmail.com>
2024-09-05 21:25:33 -0300

    create ci

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
-import {gql, useQuery} from '@apollo/client';
-import React from 'react';
-import {Lesson} from './Lesson';
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
+import { Lesson } from "./Lesson";
 
 const GET_LESSONS_QUERY = gql`
 	query  {
@@ -14,37 +14,35 @@ const GET_LESSONS_QUERY = gql`
 	}
 `;
 
-interface IGetLessonsQueryResponse  {
+interface IGetLessonsQueryResponse {
 	lessons: Array<{
 		id: string;
 		availableAt: string;
-		lessonType: 'live' | 'class';
-		slug:	string;
+		lessonType: "live" | "class";
+		slug: string;
 		title: string;
-
 	}>;
 }
 export function Sidebar() {
-	const { data 
-	
-	
-	
-	} = useQuery<IGetLessonsQueryResponse>(GET_LESSONS_QUERY);
-	console.log({data});
+	const { data } = useQuery<IGetLessonsQueryResponse>(GET_LESSONS_QUERY);
+	console.log({ data });
 
 	return (
-		<aside className='w-[348px] bg-gray-700 p-6 border-l border-gray-600'>
-			<span className='font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block'> Cronograma de aulas
+		<aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+			<span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+				{" "}
+				Cronograma de aulas
 			</span>
 
-			<div className='flex flex-col gap-8'>
-				{data?.lessons?.map(lesson => (
+			<div className="flex flex-col gap-8">
+				{data?.lessons?.map((lesson) => (
 					<Lesson
 						key={lesson?.id}
 						availableAt={new Date(lesson?.availableAt)}
 						slug={lesson?.slug}
 						title={lesson.title}
-						type={lesson?.lessonType} />
+						type={lesson?.lessonType}
+					/>
 				))}
 
 				{/* <Lesson/> */}
--- src/lib/apolo.ts
+++ src/lib/apolo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri:'https://api-us-west-2.hygraph.com/v2/cl4r00c7g0f4m01wb17yv0fby/master',
-  cache: new InMemoryCache()
-})
+	uri: "https://api-us-west-2.hygraph.com/v2/cl4r00c7g0f4m01wb17yv0fby/master",
+	cache: new InMemoryCache(),
+});
