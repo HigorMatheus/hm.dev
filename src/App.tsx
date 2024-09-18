d0a8a0d08cab1e9af78259863327a69746ef84f7
Higor Matheus <hmoliveira83@gmail.com>
2024-09-18 09:15:38 -0300

    deploy test env

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { Event } from "./pages/Event";
 
 function App() {
 	// https://api-us-west-2.hygraph.com/v2/cl4r00c7g0f4m01wb17yv0fby/master
+
+	console.log({ env: import.meta.env });
 	return (
 		<div>
 			<Header />
