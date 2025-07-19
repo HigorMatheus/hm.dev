c86b74b16d0436656ac5c6a1a6432b4a94dcc899
Higor Matheus <hmoliveira83@gmail.com>
2025-07-19 14:06:43 -0300

    ajustes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
 	return (
 		<header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
 			<Logo />{" "}
-			<h1 className="text-3xl text-green-400">deploy ci cd com sucesso</h1>
+			<h1 className="text-3xl text-green-400">deploy ci cd com sucesso test</h1>
 		</header>
 	);
 }
