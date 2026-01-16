783a1757e7dcd968026dc962aa620278802bd38d
Higor Matheus Oliveira Vieira <hmoliveira83@gmail.com>
2026-01-16 19:08:25 -0300

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 // className
 
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { Header } from "./components/Header";
 import { Event } from "./pages/Event";
 
 function App() {
 	// https://api-us-west-2.hygraph.com/v2/cl4r00c7g0f4m01wb17yv0fby/master
 
-	console.log({ env: import.meta.env });
-	return <Event />;
+	return (
+		<div className="bg-background">
+			<Header />
+			<MapContainer
+				center={[51.505, -0.09]}
+				zoom={13}
+				// scrollWheelZoom={false}
+				zoomControl
+				className="w-full h-96"
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+				<Marker position={[51.505, -0.09]}>
+					<Popup>This is a popup</Popup>
+				</Marker>
+			</MapContainer>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,7 @@ import { Logo } from "./Logo";
 
 export function Header() {
 	return (
-		<header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
-			<Logo />{" "}
+		<header className="w-full py-5 flex items-center justify-center bg-background border-b border-gray-600">
 			<h1 className="text-3xl text-green-400">deploy ci cd com sucesso test</h1>
 		</header>
 	);
